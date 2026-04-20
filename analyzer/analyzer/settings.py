1273b73bba850238bbcf6ee8791709b35a30dbe0
Divyanshu Kumar Dubey <divyanshudubey033@gmail.com>
2026-04-20 14:12:46 +0530

    Refactor Django settings to load API key from env

--- analyzer/analyzer/settings.py
+++ analyzer/analyzer/settings.py
@@ -16,7 +16,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-GEMINI_API_KEY = "AIzaSyBJBWNthlPDDB_0hprLfrXRgGe3k6w6bNc"
+
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
